dd9e330d25e80686bea0642fb455a81584c22423
andaistvan <andaistvan@gmail.com>
2018-05-06 14:02:21 +0200

    validation start
    hazahozott anyag

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,11 +1,46 @@
 <?php
 	include_once 'dbh.inc.php';
 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$ccode = $_POST['ccode'];
+	if (isset($_POST['submit'])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$ccode = $_POST['ccode'];
 
-	$sql = "INSERT INTO formdatas(name, email, ccode) VALUES ('$name', '$email', '$ccode');";
-	mysqli_query($conn, $sql);
+		//coupon code compare
+		$sql = "SELECT * FROM coupons WHERE couponcode=$ccode;";
+		$result = mysqli_query($conn, $sql);
 
-	header("Location: ../index.php?signup=succes");
+		echo 'beírt code: '. $ccode. "<br>";
+		echo 'sql var: '.$sql."<br>";
+
+
+		
+		if ($sql) {
+			echo 'fasza';
+		} else {
+			echo 'nem fasza';
+		}
+
+		// echo $sql;
+		//teszt
+		// echo $name;
+		// echo $email;
+		// echo $ccode;
+
+		//mail
+		// $mailTo = "andaistvan@gmail.com"; 
+		// $headers = "From: ".$email;
+		// $txt = "Űrlap regsiztráció: ". $name.".\n\n".$email.".\n\n".$ccode;
+
+		// mail($mailTo, $headers, $txt);
+		// header("Location: ../index.php?mailsend");
+		//mail
+	}
+
+	
+
+// validation
+// read from coupons
+	
+
+
--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>TV kupon</title>
+	<link rel="stylesheet" href="">
 </head>
 <body>
-	<form action="includes/signup.inc.php" method="POST">
-		<input type="text" name="name" placeholder="név">
-		<br>
-		<input type="text" name="email" placeholder="e-mail">
-		<br>
-		<input type="text" name="ccode" placeholder="kupon kód">
-		<br>
-		<button type="submit" name="submit">Submit</button>
-	</form>
+	<main>
+		<form class="contact-form" action="includes/signup.inc.php" method="POST">
+			<input type="text" name="name" placeholder="név">
+			<br>
+			<input type="text" name="email" placeholder="e-mail">
+			<br>
+			<input type="text" name="ccode" placeholder="kupon kód">
+			<br>
+			<button type="submit" name="submit">Submit</button>
+		</form>
+	</main>
 </body>
 </html>
